f793a77d9a5299022f98464e4d3db023fd964e09
Juan Felipe Mosquera <juanmosquera06@gmail.com>
2022-07-25 23:58:13 -0500

    Initial commit

---
+++ prestamo-api/src/test/java/co/com/meli/demo/PrestamoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.meli.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
